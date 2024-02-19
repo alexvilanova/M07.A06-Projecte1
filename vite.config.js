0ac13bada59d07bd92b2750c5ba7ea07b64ac3f5
Alex <algoar@fp.injoaquimmir.cat>
2024-02-19 19:41:32 +0100

    devolver cambios

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://m07-a06-projecte1-6o2k.vercel.app/',
     plugins: [
         laravel({
             input: [
