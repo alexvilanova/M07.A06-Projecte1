b8c1e0a218b96f4529b776f6109885a8d9c2a662
Alex <algoar@fp.insjoaquimmir.cat>
2024-02-20 12:29:22 +0100

    base vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/build/assets/',    
     plugins: [
         laravel({
             input: [
