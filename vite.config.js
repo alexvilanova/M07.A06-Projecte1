66f2077ecdaf918fb3ec69152db043d06b8521c4
Alex <algoar@fp.insjoaquimmir.cat>
2024-02-20 12:26:22 +0100

    remove vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/build/',    
     plugins: [
         laravel({
             input: [
