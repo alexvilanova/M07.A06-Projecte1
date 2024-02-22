131b223b0038dfe9b5f37f468aa51bc75f42bdc2
Alex <algoar@fp.injoaquimmir.cat>
2024-02-22 16:45:42 +0100

    FK duplicada en migración

--- database/migrations/2023_11_15_164203_create_favorits_table.php
+++ database/migrations/2023_11_15_164203_create_favorits_table.php
@@ -26,9 +26,6 @@ return new class extends Migration
 
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('place_id')->references('id')->on('places')->onDelete('cascade');
-
             $table->unique(['user_id', 'place_id']);
         });
     
